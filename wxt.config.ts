d808e6f69e9e34bdb41d6344881f62b55a5b602d
sironekotoro <8675420+sironekotoro@users.noreply.github.com>
2026-09-13 01:36:56 +0900

    chore: adopt dated extension version

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'wxt';
 
 const SHEETS_READONLY_SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 const CHROME_EXTENSION_KEY = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3Ny/S5uwAYf0feA1rgut9u6foRkdmunFx0Ea7rfUyXqfWuQh1ma22Y98ZO5q3Q0ft/+6OIbA3XcVHSSrAoWKCuPQQ52u+d/fujSyuaDoym/bS/fBJ6L0vY1hxM7RVI1/FzMX2FqWxyAdtz1DVR+TJO0V91yN8KwQUp67f9AmZUbtRMcBIpvoEKlr/3VLq4nfnP03M/eOKsCOXs3DYv0ns/bHao23x+JrSlBjvWyAtyXb25oyHGX6oTx0OMLoEMdbiavu+LbLeC4SlHqRwgvXG+Yg49gyaUOueIH8nmdjLLhpGKkDq4kuMVooN4ZDCAk7hmeghlcBgN78uWblsr6oowIDAQAB';
+const EXTENSION_VERSION = '0.4.26.913';
 
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
@@ -10,6 +11,7 @@ export default defineConfig({
 
     return {
       name: 'LookupBox',
+      version: EXTENSION_VERSION,
       description: 'Search and copy key-value reference lists from Google Sheets and other data sources.',
       permissions: ['storage', 'identity'],
       host_permissions: [
@@ -34,6 +36,6 @@ export default defineConfig({
     };
   },
   zip: {
-    artifactTemplate: 'lookup-box-{{packageVersion}}-{{browser}}.zip'
+    artifactTemplate: `lookup-box-${EXTENSION_VERSION}-{{browser}}.zip`
   }
 });
